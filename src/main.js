65245b19de8e19989e6f5973f6e95c9e35237006
Haroen Viaene <hello@haroen.me>
2016-04-29 20:43:02 +0200

    instant

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ var algoliasearch = require('algoliasearch');
 var client = algoliasearch('MO3EP03JWU', 'd7afe3e2ea005d92e76df42a6ba4bd69');
 var index = client.initIndex('visitgent');
 
-document.getElementById('search').addEventListener('submit',function(e){
+document.querySelector('[name=search]').addEventListener('input',function(e){
   e.preventDefault();
-  var form = formObj(this);
+  var form = formObj(document.getElementById('search'));
   document.getElementById('results').innerHTML = '';
   index.search(form.search, function searchDone(err, content) {
     if (err) {
@@ -15,17 +15,21 @@ document.getElementById('search').addEventListener('submit',function(e){
       for (var hit in content.hits) {
         if (content.hits.hasOwnProperty(hit) && content.hits[hit].language === form.language) {
           console.log(content.hits[hit]);
-          document.getElementById('results').innerHTML +=
+          // document.getElementById('results').innerHTML +=
+          var res = document.createElement('article');
+          res.classList.add('result');
+          res.innerHTML =
 html`
-<div class="👻"></div>
-<article class="result">
-  <img src="${content.hits[hit].images[0]}" alt="${content.hits[hit].title}" class="result--image"/>
-  <div class="result--bottom">
-    <h1 class="result--title">${content.hits[hit].title}</h1>
-    <p class="result--summary">${content.hits[hit].summary}</p>
-  </div>
-</article>
+<img src="${content.hits[hit].images[0]}" alt="${content.hits[hit].title}" class="result--image"/>
+<div class="result--bottom">
+  <h1 class="result--title">${content.hits[hit].title}</h1>
+  <p class="result--summary">${content.hits[hit].summary}</p>
+</div>
 `;
+          document.querySelector('.results').appendChild(res);
+          var flexfix = document.createElement('div');
+          flexfix.classList.add('👻');
+          document.querySelector('.results').appendChild(flexfix);
         }
       }
     }
